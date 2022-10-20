0fcc30c9737df5c406c2058f4f16c576041c0db4
Joel Mathew <mail@joelmath.io>
2022-10-19 22:11:17 -0700

    Add occursUnder file

---
+++ usfm/viz.py
@@ -0,0 +1,48 @@
+from pprint import pprint
+
+is_in_block = True
+block = {}
+markers = []
+with open("usfm.sty") as sty_file:
+    for line in sty_file:
+        line = line.strip()
+        # print(line)
+        if line.startswith("#"):
+            continue
+
+        if len(line) == 0 and is_in_block:
+            is_in_block = False
+            if len(block) > 0:
+                markers.append(block)
+                block = {}
+
+        if line.startswith("\\"):
+            # print(line)
+            is_in_block = True
+            splits = line.split(" ", maxsplit=1)
+            if len(splits) == 2:
+                key, value = splits
+                block[key[1:]] = value
+
+pprint(len(markers))
+
+with open("nodes.csv", "w") as nodes_file, open("relations.csv", "w") as relations_file:
+    # Write headers
+    nodes_file.write("markerId:ID,styleType,rank:int,:LABEL\n")
+    relations_file.write(":START_ID,:END_ID,:TYPE\n")
+    for marker in markers:
+        rank = ""
+        if "Rank" in marker:
+            rank = marker["Rank"]
+        nodes_file.write(f"{marker['Marker']},{marker['StyleType']},{rank},Marker\n")
+        if "OccursUnder" in marker:
+            for m in marker["OccursUnder"].split():
+                relations_file.write(f"{m},{marker['Marker']},OCCURS_UNDER\n")
+
+# with open("output.mermaid", "w") as mermaid_file:
+#     for marker in markers:
+#         if "OccursUnder" not in marker:
+#             mermaid_file.write(f"{marker['Marker']}\n")
+#         else:
+#             for m in marker["OccursUnder"].split():
+#                 mermaid_file.write(f"{m} --> {marker['Marker']}\n")
